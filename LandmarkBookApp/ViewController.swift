07448ad6d79ae6b4976d45563b3c3967b05a0d6e
Felipe Augusto <augusto_felipe@icloud.com>
2022-08-02 18:55:59 -0300

    First Commit

--- LandmarkBookApp/ViewController.swift
+++ LandmarkBookApp/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        var landmarkNames = [String]()
+        landmarkNames.append("coliseum")
+        landmarkNames.append("taj-mahal")
+        
+        var landmarkImages = [UIImage]()
+        landmarkImages.append(UIImage(named: "coliseum")!)
+        landmarkImages.append(UIImage(named: "taj-mahal")!)
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 }
 
